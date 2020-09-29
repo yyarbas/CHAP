759440254c26fda261d9d4dd7752c5c84891d673
Yakup <yakupyarbas@outlook.de>
2020-09-29 17:12:33 +0200

    Implemented client and server hash comparison as function to the server. Implented User Menu to the client.

--- RPC/RPC-Client.py
+++ RPC/RPC-Client.py
@@ -2,22 +2,80 @@
 
 import xmlrpc.client
 import hashlib
+import time
 from getpass import getpass
 
 server = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-#Show Methods of running Server
-print(server.system.listMethods())
+while True:
+	print('\033[H\033[J')
+	print(50*'-')
+	print('MENU')
+	print(50*'-')
+	print('1. LogIn')
+	print('2. Exit')
+	print(50*'-')
 
-print("LogIn")
+	print('Enter a Number')
+	user_input = input('Your choice: ')
+	if user_input == '1':
+		print("LogIn")
 
-username = input("Username:")
-password = str(getpass())
+		username = input("Username:")
+		password = str(getpass())
 	
-token = server.auth(username)
-print(token[0])
-print(token[1])
-password_token = password + token[0]
-password_token_hash = hashlib.md5(password_token.encode('utf-8')).hexdigest()
-if token[1] == password_token_hash:
-	
+		token = server.authenticate(username)
+		password_token = password + token
+		password_token_hash = hashlib.md5(password_token.encode('utf-8')).hexdigest()
+		authorized_successfull = server.authorize(username, password, password_token_hash)
+
+		if authorized_successfull == 1:
+			
+				print('authorized for premium functions')
+				time.sleep(3)
+				while(user_input == '1'):
+					print('\033[H\033[J')
+					print('1. Pow')
+					print('2. Divide')
+					print('3. LogOut')
+
+					print('Enter a Number')
+					premium_function_input = input('Your Choice: ')
+
+					if premium_function_input == '1':
+						first_entry = int(input('First Number: '))
+						second_entry = int(input('Second Number: '))
+						result = server.pow(first_entry,second_entry)
+						print('The result is ' + str(result))
+						time.sleep(3)
+
+					elif premium_function_input == '2':
+						first_entry = int(input('First Number: '))
+						second_entry = int(input('Second Number: '))
+						result = server.div(first_entry,second_entry)
+						print('The result is ' + str(result))
+						time.sleep(3)
+
+					elif premium_function_input == '3':
+						break
+					
+					print('\033[H\033[J')
+					print('Do want to do another Operation?')
+					print('1.Yes')
+					print('2.No')
+					print('Enter a Number')
+					print('Your Choice: ')
+					user_input = input()
+			
+
+		if authorized_successfull != 1:
+			print('access denied')
+
+	elif user_input == '2':
+		exit()
+
+	else:
+		print('\033[H\033[J') 
+		print('\033[4m' + 'Invalid choice!' + '\033[0m' )
+		print('Try Again!')
+		time.sleep(5)	
